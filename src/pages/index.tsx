b3641b7973929bcb201510c94249971cfbeb4078
Juan <34592028+xMinota@users.noreply.github.com>
2022-05-17 16:05:16 +0000

    :rocket: Create a landing page for the website.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
-import { Button } from "@blueprintjs/core";
-
 export default function() {
     return (
         <>
-            
+            <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+                <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+                    <h1 className="text-xl font-bold">
+                    👺 Akuma
+                    </h1>
+                    <h2 className="text-lg">A classy comic/manga reader application.</h2>
+                    <br />
+                    <hr />
+                    <br />
+                    <p>
+                        <a href="https://discord.gg/rfC6MUzU" className="font-bold text-sky-500 hover:text-sky-600">We're not done yet! Check out the discord server for more updates &rarr;</a>
+                    </p>
+                </div>
+            </div>
         </>
     )
 }
